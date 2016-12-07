61a3af0501944e70ad2488ae4e2338b37f662a31
scubamut <scubamut@gmail.com>
2016-12-07 10:33:46 +0200

    capitalized 'period' to be consistent

--- backtest_helpers/endpoints.py
+++ backtest_helpers/endpoints.py
@@ -1,9 +1,6 @@
-def endpoints(start=None, end=None, period='m', trading_days=None):
+def endpoints(start=None, end=None, period='M', trading_days=None):
     if trading_days is not None:
         dates = trading_days
-    # the following 2 lines cause python 3.4.2 to crash, so removed them
-    #    elif start is not None and end is not None:
-    #        dates = tradingcalendar.get_trading_days(start, end)
     else:
         print('\n** ERROR : must either provide pandas series (or df) of trading days \n')
         print('           or a start and end date\n')
@@ -11,13 +8,13 @@ def endpoints(start=None, end=None, period='m', trading_days=None):
     if isinstance(period, int):
         dates = [dates[i] for i in range(0, len(dates), period)]
     else:
-        if period == 'm':
+        if period == 'M':
             months = 1
-        elif period == 'q':
+        elif period == 'Q':
             months = 3
-        elif period == 'b':
+        elif period == 'B':
             months = 6
-        elif period == 'y':
+        elif period == 'Y':
             months = 12
 
         e_dates = [dates[i - 1] for i in range(1, len(dates)) \
