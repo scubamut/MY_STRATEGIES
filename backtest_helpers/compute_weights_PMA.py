f3d0c9fe8342787efc7525b37a58ce7a7f42f28b
Dave Gilbert <scubamut@gmail.com>
2017-01-03 12:59:59 +0000

    refactor

--- backtest_helpers/compute_weights_PMA.py
+++ backtest_helpers/compute_weights_PMA.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from .Parameters import Parameters
+from .get_prices import get_prices
 from .endpoints import endpoints
 from .backtest import backtest
 from .get_yahoo_data import get_yahoo_data
@@ -7,33 +9,23 @@ def compute_weights_PMA(name, parameters):
 
     print(name)
 
-    symbols = parameters['symbols']
-    cash_proxy = parameters['cash_proxy']
-    prices = parameters['prices']
+    p = Parameters(parameters)
 
-    risk_lookback = parameters['risk_lookback']
-    allocations = parameters['allocations']
-    frequency = parameters['frequency']
+    prices = get_prices(p)
 
-    tickers = list(set(symbols + [cash_proxy]))
-    if isinstance(prices, str):
-        if prices == 'yahoo':
-            data = get_yahoo_data(tickers)
-            prices = data.copy().dropna()
-
-    end_points = endpoints(period=frequency, trading_days=prices.index)
+    end_points = endpoints(period=p.frequency, trading_days=prices.index)
     prices_m = prices.loc[end_points]
 
     # elligibility rule
-    SMA = prices_m.rolling(risk_lookback).mean().dropna()
+    SMA = prices_m.rolling(p.risk_lookback).mean().dropna()
     rebalance_dates = SMA.index
-    rule = prices_m.loc[rebalance_dates][symbols] > SMA[symbols]
+    rule = prices_m.loc[rebalance_dates][p.symbols] > SMA[p.symbols]
 
     # fixed weight allocation
-    weights = allocations * rule
+    weights = p.allocations * rule
 
     # downside protection
-    weights[cash_proxy] = 1 - weights[symbols].sum(axis=1)
+    weights[p.cash_proxy] = 1 - weights[p.symbols].sum(axis=1)
 
     # backtest
     p_value, p_holdings, p_weights = backtest(prices, weights, 10000., offset=0, commission=10.)
--- backtest_helpers/compute_weights_RS_DM.py
+++ backtest_helpers/compute_weights_RS_DM.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from .Parameters import Parameters
+from .get_prices import get_prices
 from .endpoints import endpoints
 from .backtest import backtest
 from .get_yahoo_data import get_yahoo_data
@@ -7,59 +9,40 @@ def compute_weights_RS_DM(name, parameters):
 
     print('Strategy : {}'.format(name))
 
-    symbols = parameters['symbols']
-    cash_proxy = parameters['cash_proxy']
-    risk_free = parameters['risk_free']
-    prices = parameters['prices']
+    p = Parameters(parameters)
 
-    rs_lookback = parameters['rs_lookback']
-    risk_lookback = parameters['risk_lookback']
-    n_top = parameters['n_top']
+    prices = get_prices(p)
 
-    frequency = parameters['frequency']
-
-    if isinstance(prices, str):
-        if prices == 'yahoo':
-            tickers = symbols.copy()
-            if cash_proxy != 'CASHX':
-                tickers = list(set(tickers + [cash_proxy]))
-            if isinstance(risk_free, str):
-                tickers = list(set(tickers + [risk_free]))
-
-            data = get_yahoo_data(tickers)
-
-            prices = data.copy().dropna()
-
-    end_points = endpoints(period=frequency, trading_days=prices.index)
+    end_points = endpoints(period=p.frequency, trading_days=prices.index)
     prices_m = prices.loc[end_points]
 
-    returns = prices_m[symbols].pct_change(rs_lookback)[rs_lookback:]
+    returns = prices_m[p.symbols].pct_change(p.rs_lookback)[p.rs_lookback:]
 
-    if isinstance(risk_free, int):
+    if isinstance(p.risk_free, int):
         excess_returns = returns
     else:
-        risk_free_returns = prices_m[risk_free].pct_change(rs_lookback)[rs_lookback:]
+        risk_free_returns = prices_m[p.risk_free].pct_change(p.rs_lookback)[p.rs_lookback:]
         excess_returns = returns.subtract(risk_free_returns, axis=0).dropna()
 
-    absolute_momentum = prices_m[symbols].pct_change(risk_lookback)[risk_lookback:]
+    absolute_momentum = prices_m[p.symbols].pct_change(p.risk_lookback)[p.risk_lookback:]
     absolute_momentum_rule = absolute_momentum > 0
     rebalance_dates = excess_returns.index.join(absolute_momentum_rule.index, how='inner')
 
     # relative strength ranking
-    ranked = excess_returns.loc[rebalance_dates][symbols].rank(ascending=False, axis=1, method='dense')
+    ranked = excess_returns.loc[rebalance_dates][p.symbols].rank(ascending=False, axis=1, method='dense')
     # elligibility rule - top n_top ranked securities
-    elligible = ranked[ranked <= n_top] > 0
+    elligible = ranked[ranked <= p.n_top] > 0
 
     # equal weight allocations
     elligible = elligible.multiply(1. / elligible.sum(1), axis=0)
 
     # downside protection
     weights = pd.DataFrame(0., index=elligible.index, columns=prices.columns)
-    if cash_proxy == 'CASHX':
-        weights[cash_proxy] = 0
-        prices[cash_proxy] = 1.
-    weights[symbols] = (elligible * absolute_momentum_rule).dropna()
-    weights[cash_proxy] += 1 - weights[symbols].sum(axis=1)
+    if p.cash_proxy == 'CASHX':
+        weights[p.cash_proxy] = 0
+        prices[p.cash_proxy] = 1.
+    weights[p.symbols] = (elligible * absolute_momentum_rule).dropna()
+    weights[p.cash_proxy] += 1 - weights[p.symbols].sum(axis=1)
 
     # backtest
 
