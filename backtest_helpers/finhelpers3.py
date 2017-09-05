f5652df258301047508b89633f8f9c6550021b28
scubamut <scubamut@gmail.com>
2017-09-05 17:15:54 +0200

    routines to show_annual_returns including colors

--- backtest_helpers/finhelpers3.py
+++ backtest_helpers/finhelpers3.py
@@ -519,7 +519,7 @@ def show_return_table(strategy_value):
     return df.style.apply(highlight_pos_neg)
 
 def show_annual_returns(strategy_value) :
-    df = show_return_table(strategy_value)
+    df = monthly_return_table (strategy_value)
     frame = df['Annual Returns'].to_frame()
     frame['positive'] = df['Annual Returns'] >= 0
     frame['Annual Returns'].plot(figsize=(15, 10), kind='bar', color=frame.positive.map({True: 'g', False: 'r'}), grid=True)
